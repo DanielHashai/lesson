4a987125b1ec622a7d474b384459adde2b252674
DanielHashai <aaronahashai@gmail.com>
2021-11-04 19:41:51 +0200

    second try

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "Func.h"
+
+int main(){
+
+  int n1 =  max(3,5);
+  int n2 =  min(3,5);
+  printf("the big num is: %d",n1);
+   printf("the small num is: %d",n2);
+    return 0;
+
+}
---
+++ max.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "Func.h"
+
+int max(int i,int j){
+    if(i>j)
+    return i;
+    else
+    return j;
+    //printf("The min value is : %d",(i>j) ? 'x':'y');
+}
